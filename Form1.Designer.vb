c49cfdd3fcbda8769efb1913788012aa5db68efa
PIYUSH MAKWANA <mpiyush3510@cbigujarat.onmicrosoft.com>
2023-02-13 16:28:13 +0530

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,240 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.ButtonUpdate = New System.Windows.Forms.Button()
+        Me.ButtonRefresh = New System.Windows.Forms.Button()
+        Me.ButtonDelete = New System.Windows.Forms.Button()
+        Me.ButtonSave = New System.Windows.Forms.Button()
+        Me.ComboBoxType = New System.Windows.Forms.ComboBox()
+        Me.TextBoxRate = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.TextBoxCompany = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBoxName = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBoxMid = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.DataGridViewReader = New System.Windows.Forms.DataGridView()
+        Me.ButtonClear = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.DataGridViewReader, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.ButtonClear)
+        Me.GroupBox1.Controls.Add(Me.ButtonUpdate)
+        Me.GroupBox1.Controls.Add(Me.ButtonRefresh)
+        Me.GroupBox1.Controls.Add(Me.ButtonDelete)
+        Me.GroupBox1.Controls.Add(Me.ButtonSave)
+        Me.GroupBox1.Controls.Add(Me.ComboBoxType)
+        Me.GroupBox1.Controls.Add(Me.TextBoxRate)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.TextBoxCompany)
+        Me.GroupBox1.Controls.Add(Me.Label6)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.TextBoxName)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.TextBoxMid)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(63, 56)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(1272, 552)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Mahavir Medical "
+        '
+        'ButtonUpdate
+        '
+        Me.ButtonUpdate.Location = New System.Drawing.Point(93, 430)
+        Me.ButtonUpdate.Name = "ButtonUpdate"
+        Me.ButtonUpdate.Size = New System.Drawing.Size(175, 46)
+        Me.ButtonUpdate.TabIndex = 17
+        Me.ButtonUpdate.Text = "Update"
+        Me.ButtonUpdate.UseVisualStyleBackColor = True
+        '
+        'ButtonRefresh
+        '
+        Me.ButtonRefresh.Location = New System.Drawing.Point(653, 358)
+        Me.ButtonRefresh.Name = "ButtonRefresh"
+        Me.ButtonRefresh.Size = New System.Drawing.Size(175, 46)
+        Me.ButtonRefresh.TabIndex = 16
+        Me.ButtonRefresh.Text = "Refresh"
+        Me.ButtonRefresh.UseVisualStyleBackColor = True
+        '
+        'ButtonDelete
+        '
+        Me.ButtonDelete.Location = New System.Drawing.Point(953, 430)
+        Me.ButtonDelete.Name = "ButtonDelete"
+        Me.ButtonDelete.Size = New System.Drawing.Size(175, 46)
+        Me.ButtonDelete.TabIndex = 15
+        Me.ButtonDelete.Text = "Delete"
+        Me.ButtonDelete.UseVisualStyleBackColor = True
+        '
+        'ButtonSave
+        '
+        Me.ButtonSave.Location = New System.Drawing.Point(395, 358)
+        Me.ButtonSave.Name = "ButtonSave"
+        Me.ButtonSave.Size = New System.Drawing.Size(175, 46)
+        Me.ButtonSave.TabIndex = 14
+        Me.ButtonSave.Text = "Save"
+        Me.ButtonSave.UseVisualStyleBackColor = True
+        '
+        'ComboBoxType
+        '
+        Me.ComboBoxType.FormattingEnabled = True
+        Me.ComboBoxType.Items.AddRange(New Object() {"- Choose Item -", "tablet", "capsule", "syrup "})
+        Me.ComboBoxType.Location = New System.Drawing.Point(207, 243)
+        Me.ComboBoxType.Name = "ComboBoxType"
+        Me.ComboBoxType.Size = New System.Drawing.Size(242, 40)
+        Me.ComboBoxType.TabIndex = 12
+        '
+        'TextBoxRate
+        '
+        Me.TextBoxRate.Location = New System.Drawing.Point(848, 170)
+        Me.TextBoxRate.Name = "TextBoxRate"
+        Me.TextBoxRate.Size = New System.Drawing.Size(280, 39)
+        Me.TextBoxRate.TabIndex = 9
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(712, 170)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(61, 32)
+        Me.Label5.TabIndex = 8
+        Me.Label5.Text = "Rate"
+        '
+        'TextBoxCompany
+        '
+        Me.TextBoxCompany.Location = New System.Drawing.Point(848, 102)
+        Me.TextBoxCompany.Name = "TextBoxCompany"
+        Me.TextBoxCompany.Size = New System.Drawing.Size(280, 39)
+        Me.TextBoxCompany.TabIndex = 7
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(712, 102)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(116, 32)
+        Me.Label6.TabIndex = 6
+        Me.Label6.Text = "Company"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(93, 243)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(65, 32)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Type"
+        '
+        'TextBoxName
+        '
+        Me.TextBoxName.Location = New System.Drawing.Point(207, 177)
+        Me.TextBoxName.Name = "TextBoxName"
+        Me.TextBoxName.Size = New System.Drawing.Size(412, 39)
+        Me.TextBoxName.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(93, 177)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(78, 32)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Name"
+        '
+        'TextBoxMid
+        '
+        Me.TextBoxMid.Location = New System.Drawing.Point(207, 109)
+        Me.TextBoxMid.Name = "TextBoxMid"
+        Me.TextBoxMid.Size = New System.Drawing.Size(231, 39)
+        Me.TextBoxMid.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(93, 109)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(56, 32)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Mid"
+        '
+        'DataGridViewReader
+        '
+        Me.DataGridViewReader.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridViewReader.Location = New System.Drawing.Point(63, 628)
+        Me.DataGridViewReader.Name = "DataGridViewReader"
+        Me.DataGridViewReader.RowHeadersWidth = 82
+        Me.DataGridViewReader.RowTemplate.Height = 41
+        Me.DataGridViewReader.Size = New System.Drawing.Size(1272, 597)
+        Me.DataGridViewReader.TabIndex = 1
+        '
+        'ButtonClear
+        '
+        Me.ButtonClear.Location = New System.Drawing.Point(510, 430)
+        Me.ButtonClear.Name = "ButtonClear"
+        Me.ButtonClear.Size = New System.Drawing.Size(175, 46)
+        Me.ButtonClear.TabIndex = 18
+        Me.ButtonClear.Text = "Clear"
+        Me.ButtonClear.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(13.0!, 32.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1417, 1303)
+        Me.Controls.Add(Me.DataGridViewReader)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        CType(Me.DataGridViewReader, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents TextBoxName As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBoxMid As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents DataGridViewReader As DataGridView
+    Friend WithEvents Label3 As Label
+    Friend WithEvents ButtonUpdate As Button
+    Friend WithEvents ButtonRefresh As Button
+    Friend WithEvents ButtonDelete As Button
+    Friend WithEvents ButtonSave As Button
+    Friend WithEvents ComboBoxType As ComboBox
+    Friend WithEvents TextBoxRate As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents TextBoxCompany As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents ButtonClear As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,202 @@
+﻿Imports System.Data.SqlClient
+
+Public Class Form1
+    Dim conStr As String = "Data Source=MPIYUSH3510-AMD;Initial Catalog=Medical;Integrated Security=true"
+    Dim sqlCmd As SqlCommand
+    Dim Connection As SqlConnection
+    Dim dtAdapater As SqlDataAdapter
+    Dim dtSet As DataSet
+    Private Sub ButtonSave_Click(sender As Object, e As EventArgs) Handles ButtonSave.Click
+        Connection = New SqlConnection("Data Source=MPIYUSH3510-AMD;Initial Catalog=Medical;Integrated Security=true")
+        Try
+            Connection.Open()
+            If Connection.State = ConnectionState.Open Then
+                ' MsgBox("Connected To Medical Database !")
+            End If
+        Catch ec As Exception
+            MsgBox(ec.Message)
+        End Try
+
+        If TextBoxMid.Text = "" And TextBoxName.Text = "" And ComboBoxType.Text = "- Choose Item -" And TextBoxCompany.Text = "" And TextBoxRate.Text = "" Then
+            MsgBox("All fields are empty", "failed")
+            Return
+        End If
+
+        If TextBoxMid.Text = "" Then
+            MsgBox("Please Enter Mid !", "failed")
+            Return
+        End If
+
+        If TextBoxName.Text = "" Then
+            MsgBox("Name can't be empty !", "failed")
+            Return
+        End If
+
+        If ComboBoxType.Text = "" Then
+            MsgBox("Please select correctly !", "failed")
+            Return
+        End If
+
+        If TextBoxCompany.Text = "" Then
+            MsgBox("Company can't choose  !", "failed")
+            Return
+        End If
+
+        If TextBoxRate.Text = "" Then
+            MsgBox("Rate Can't be empty !", "failed")
+            Return
+        End If
+
+        CreatetblMedicine()
+        InsertIntotblMedicine()
+        ButtonClear_Click(sender, e)
+        ShowDataFromtblMedicine()
+    End Sub
+
+    Public Sub CreatetblMedicine()
+        Try
+            sqlCmd = New SqlCommand("Create table tblMedicine(mid int Unique,name varchar(40),type varchar(10),company varchar(20),rate int)", Connection)
+            sqlCmd.ExecuteNonQuery()
+            'MsgBox("Table Created !")
+        Catch ec As Exception
+            MsgBox(ec.Message)
+        End Try
+    End Sub
+
+    Public Sub InsertIntotblMedicine()
+        Try
+            Dim mid As Integer = Convert.ToInt32(TextBoxMid.Text)
+            Dim name As String = TextBoxName.Text
+            Dim type As String = ComboBoxType.Text
+            Dim company As String = TextBoxCompany.Text
+            Dim rate As Integer = Convert.ToInt32(TextBoxRate.Text)
+
+            sqlCmd = New SqlCommand("insert into tblMedicine values(@mid,@name,@type,@company,@rate)", Connection)
+            sqlCmd.Parameters.AddWithValue("@mid", mid)
+            sqlCmd.Parameters.AddWithValue("@name", name)
+            sqlCmd.Parameters.AddWithValue("@type", type)
+            sqlCmd.Parameters.AddWithValue("@company", company)
+            sqlCmd.Parameters.AddWithValue("@rate", rate)
+            sqlCmd.ExecuteNonQuery()
+            MsgBox("Inserted !")
+        Catch ec As Exception
+            MsgBox(ec.Message)
+        End Try
+    End Sub
+
+    Public Sub ShowDataFromtblMedicine()
+        Try
+            dtAdapater = New SqlDataAdapter("select * from tblMedicine order by mid", Connection)
+            dtSet = New DataSet()
+            dtAdapater.Fill(dtSet, "tblMedicine")
+            DataGridViewReader.DataSource = dtSet.Tables(0).DefaultView
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub ButtonRefresh_Click(sender As Object, e As EventArgs) Handles ButtonRefresh.Click
+        Connection = New SqlConnection(conStr)
+        Connection.Open()
+        Try
+            dtAdapater = New SqlDataAdapter("select * from tblMedicine order by mid", Connection)
+            dtSet = New DataSet()
+            dtAdapater.Fill(dtSet, "tblMedicine")
+            DataGridViewReader.DataSource = dtSet.Tables(0).DefaultView
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub ButtonUpdate_Click(sender As Object, e As EventArgs) Handles ButtonUpdate.Click
+        If TextBoxMid.Text = "" Then
+            MsgBox("Please Enter Mid !", "failed")
+            Return
+        End If
+
+        If TextBoxName.Text = "" Then
+            MsgBox("Name can't be empty !", "failed")
+            Return
+        End If
+
+        If ComboBoxType.Text = "" Then
+            MsgBox("Please select correctly !", "failed")
+            Return
+        End If
+
+        If TextBoxCompany.Text = "" Then
+            MsgBox("Company can't choose  !", "failed")
+            Return
+        End If
+
+        If TextBoxRate.Text = "" Then
+            MsgBox("Rate Can't be empty !", "failed")
+            Return
+        End If
+
+        Try
+            Dim mid As Integer = Convert.ToInt32(TextBoxMid.Text)
+            Dim name As String = TextBoxName.Text
+            Dim type As String = ComboBoxType.Text
+            Dim company As String = TextBoxCompany.Text
+            Dim rate As Integer = Convert.ToInt32(TextBoxRate.Text)
+
+            sqlCmd = New SqlCommand("Update tblMedicine set name=@name,type=@type,company=@company,rate=@rate where mid=@mid", Connection)
+            sqlCmd.Parameters.AddWithValue("@name", name)
+            sqlCmd.Parameters.AddWithValue("@type", type)
+            sqlCmd.Parameters.AddWithValue("@company", company)
+            sqlCmd.Parameters.AddWithValue("@rate", rate)
+            sqlCmd.Parameters.AddWithValue("@mid", mid)
+            sqlCmd.ExecuteNonQuery()
+            MsgBox("Update !")
+
+            ButtonRefresh_Click(sender, e)
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+        ButtonClear_Click(sender, e)
+    End Sub
+
+    Private Sub ButtonDelete_Click(sender As Object, e As EventArgs) Handles ButtonDelete.Click
+        Connection = New SqlConnection(conStr)
+        Connection.Open()
+
+        If DataGridViewReader.RowCount = 0 Then
+            MsgBox("Unable to deleted, No Data found !", MsgBoxStyle.Critical, "failed")
+            Return
+        End If
+
+        If MsgBox("Delete This Record ?", MsgBoxStyle.Question + MsgBoxStyle.OkCancel, "Confirmation") Then Return
+
+        Try
+            If DataGridViewReader.AreAllCellsSelected(0) = True Then
+                sqlCmd = New SqlCommand("deleted from tblMedicine", Connection)
+                sqlCmd.ExecuteNonQuery()
+            End If
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+
+        Try
+            For Each row As DataGridViewRow In DataGridViewReader.SelectedCells
+                If row.Selected Then
+                    sqlCmd = New SqlCommand("delete from tblMedicine where mid = '" & row.DataBoundItem(0).ToString & "'", Connection)
+                    sqlCmd.ExecuteNonQuery()
+                End If
+            Next
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+        ButtonRefresh_Click(sender, e)
+        ButtonClear_Click(sender, e)
+    End Sub
+
+    Private Sub ButtonClear_Click(sender As Object, e As EventArgs) Handles ButtonClear.Click
+        TextBoxMid.Clear()
+        TextBoxName.Clear()
+        ComboBoxType.SelectedItem = "- Choose Item -"
+        TextBoxRate.Clear()
+        TextBoxCompany.Clear()
+    End Sub
+End Class
